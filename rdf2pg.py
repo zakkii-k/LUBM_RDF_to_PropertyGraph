ec7666a7d04a4b97f911b2d3016ec6be1b537e69
zakkii-k <59763423+zakkii-k@users.noreply.github.com>
2024-07-16 15:38:15 +0900

    modify python file

--- rdf2pg.py
+++ rdf2pg.py
@@ -6,8 +6,7 @@ import tqdm
 import argparse
 
 
-def main():
-    # Setting command line arguments
+def parse_arguments():
     parser = argparse.ArgumentParser(description="RDF to Property Graph Converter")
     parser.add_argument("-o", "--owl_file", required=True, help="Path to the OWL file")
     parser.add_argument(
@@ -26,36 +25,38 @@ def main():
         default=10000,
         help="Chunk size for JSON files (default: 10000)",
     )
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    # Setting file paths
-    owl_file = args.owl_file
-    ntriples_dir = args.ntriples_dir
-    json_dir_path = args.json_dir_path
 
-    # Loading OWL graph
+def load_owl_graph(owl_file):
     owl_graph = rdflib.Graph()
     owl_graph.parse(owl_file, format="xml")
+    return owl_graph
 
-    # Loading RDF graph
+
+def load_ntriples_graph(ntriples_dir):
+    print(f"===== loading ntriples graph =====")
     g = rdflib.Graph()
-
-    print("===== reading ntriples files =====")
-    for file in tqdm.tqdm(glob.glob(ntriples_dir)):
+    for file in tqdm.tqdm(glob.glob(os.path.join(ntriples_dir, "*.nt"))):
         g.parse(file, format="nt")
+    return g
 
-    # Extracting class information
+
+def replace_https_with_http(uri):
+    if str(uri).startswith("https://"):
+        return rdflib.URIRef(str(uri).replace("https://", "http://"))
+    return uri
+
+
+def extract_class_info(owl_graph):
+    print(f"===== extracting class info =====")
     class_labels = {}
     dataTypeProperty = {}
     objectProperty = {}
-    print("===== extracting class information =====")
     for subj, pred, obj in tqdm.tqdm(owl_graph):
-        if str(subj).startswith("https://"):
-            subj = rdflib.URIRef(str(subj).replace("https://", "http://"))
-        if str(pred).startswith("https://"):
-            pred = rdflib.URIRef(str(pred).replace("https://", "http://"))
-        if str(obj).startswith("https://"):
-            obj = rdflib.URIRef(str(obj).replace("https://", "http://"))
+        subj = replace_https_with_http(subj)
+        pred = replace_https_with_http(pred)
+        obj = replace_https_with_http(obj)
 
         if pred == rdflib.RDF.type and obj == rdflib.OWL.Class:
             class_labels[str(subj)] = str(subj).split("#")[-1]
@@ -65,50 +66,40 @@ def main():
             obj == rdflib.OWL.ObjectProperty or obj == rdflib.OWL.TransitiveProperty
         ):
             objectProperty[str(subj)] = str(subj).split("#")[-1]
-        else:
-            print("subj:", subj, "pred:", pred, "obj:", obj)
+    return class_labels, dataTypeProperty, objectProperty
 
-    # Maps for property graph
+
+def process_node(uri, node_id_map, node_map, next_node_id):
+    if uri not in node_id_map:
+        node_id_map[uri] = next_node_id
+        node_map[next_node_id] = {"id": next_node_id, "property": {"uri": uri}}
+        next_node_id += 1
+    return node_id_map[uri], next_node_id
+
+
+def construct_property_graph(g, class_labels, dataTypeProperty, objectProperty):
+    print(f"===== constructing property graph =====")
     node_map = {}
     edge_map = {}
-    # Data and id maps
     node_id_map = {}
     edge_id_map = {}
     next_node_id = 1
     next_edge_id = 1
 
-    print("===== constructing node_map and edge_map =====")
     for subj, pred, obj in tqdm.tqdm(g):
-        if str(subj).startswith("https://"):
-            subj = rdflib.URIRef(str(subj).replace("https://", "http://"))
-        if str(pred).startswith("https://"):
-            pred = rdflib.URIRef(str(pred).replace("https://", "http://"))
-        if str(obj).startswith("https://"):
-            obj = rdflib.URIRef(str(obj).replace("https://", "http://"))
+        subj = replace_https_with_http(subj)
+        pred = replace_https_with_http(pred)
+        obj = replace_https_with_http(obj)
         subj = str(subj)
         pred = str(pred)
         obj = str(obj)
         if "file://" in subj or "file://" in pred or "file://" in obj:
             continue
-        sub_id = -1
-        obj_id = -1
-        if subj not in node_id_map:  # When subj appears for the first time
-            node_id_map[subj] = next_node_id
-            node_map[next_node_id] = {"id": next_node_id, "property": {"uri": subj}}
-            sub_id = next_node_id
-            next_node_id += 1
-        else:
-            sub_id = node_id_map[subj]
 
-        if pred in objectProperty:  # When pred acts as an edge in the property graph
-            if obj not in node_id_map:  # When obj appears for the first time
-                node_id_map[obj] = next_node_id
-                node_map[next_node_id] = {"id": next_node_id, "property": {"uri": obj}}
-                obj_id = next_node_id
-                next_node_id += 1
-            else:
-                obj_id = node_id_map[obj]
+        sub_id, next_node_id = process_node(subj, node_id_map, node_map, next_node_id)
+        obj_id, next_node_id = process_node(obj, node_id_map, node_map, next_node_id)
 
+        if pred in objectProperty:
             if (pred, sub_id, obj_id) not in edge_id_map:
                 edge_id_map[(pred, sub_id, obj_id)] = next_edge_id
                 edge_map[next_edge_id] = {
@@ -120,42 +111,42 @@ def main():
                 }
                 next_edge_id += 1
 
-        elif pred in dataTypeProperty:  # Property
+        elif pred in dataTypeProperty:
             node_map[sub_id]["property"][dataTypeProperty[pred]] = obj
         elif "type" in str(pred):
             if "label" in node_map[sub_id]:
                 node_map[sub_id]["label"].append(class_labels[obj])
             else:
                 node_map[sub_id]["label"] = [class_labels[obj]]
-        else:
-            print("sub:", subj, "pred:", pred, "obj:", obj)
 
-    # Create directory if it does not exist
-    if not os.path.exists(json_dir_path):
-        os.makedirs(json_dir_path)
+    return node_map, edge_map
 
-    # Convert node_map to list format
+
+def write_json_in_chunks(data, file_prefix, dir_path, chunk_size=10000):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    print(f"===== writing {file_prefix} files =====")
+    for i in tqdm.tqdm(range(0, len(data), chunk_size)):
+        chunk = data[i : i + chunk_size]
+        file_path = os.path.join(dir_path, f"{file_prefix}_{i // chunk_size + 1}.json")
+        with open(file_path, "w") as file:
+            json.dump(chunk, file, ensure_ascii=False, indent=4)
+
+
+def main():
+    args = parse_arguments()
+    owl_graph = load_owl_graph(args.owl_file)
+    rdf_graph = load_ntriples_graph(args.ntriples_dir)
+    class_labels, dataTypeProperty, objectProperty = extract_class_info(owl_graph)
+    node_map, edge_map = construct_property_graph(
+        rdf_graph, class_labels, dataTypeProperty, objectProperty
+    )
+
     node_list = list(node_map.values())
-
-    # Convert edge_map to list format
     edge_list = list(edge_map.values())
 
-    def write_json_in_chunks(data, file_prefix, dir_path, chunk_size=10000):
-        print(f"===== writing {file_prefix} files =====")
-        for i in tqdm.tqdm(range(0, len(data), chunk_size)):
-            chunk = data[i : i + chunk_size]
-            file_path = os.path.join(
-                dir_path, f"{file_prefix}_{i // chunk_size + 1}.json"
-            )
-            with open(file_path, "w") as file:
-                json.dump(chunk, file, ensure_ascii=False, indent=4)
-
-    print("===== writing json files =====")
-    # Split node_list and output in json format
-    write_json_in_chunks(node_list, "nodes", json_dir_path)
-
-    # Split edge_list and output in json format
-    write_json_in_chunks(edge_list, "edges", json_dir_path)
+    write_json_in_chunks(node_list, "nodes", args.json_dir_path, args.chunk_size)
+    write_json_in_chunks(edge_list, "edges", args.json_dir_path, args.chunk_size)
 
 
 if __name__ == "__main__":
--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-javac edu/lehigh/swat/bench/uba/Generator.java
-
-java edu.lehigh.swat.bench.uba.Generator -univ ${1:1} -onto https://swat.cse.lehigh.edu/onto/univ-bench.owl
+python3 rdf2pg.py -o /path/to/univ_bench.owl -nt /path/to/ntriples -j /path/to/json -c chunk_size
