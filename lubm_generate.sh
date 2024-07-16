68921802bd9ba208f05ffb9fb51bfce054f5ffd5
zakkii-k <59763423+zakkii-k@users.noreply.github.com>
2024-07-16 17:17:01 +0900

    modify lubm_generate.sh

--- lubm_generate.sh
+++ lubm_generate.sh
@@ -1,3 +1,16 @@
-javac edu/lehigh/swat/bench/uba/Generator.java
+#!/bin/bash
 
-java edu.lehigh.swat.bench.uba.Generator -univ ${1:1} -onto https://swat.cse.lehigh.edu/onto/univ-bench.owl
+DEFAULT_UNIV=1
+
+UNIV=${1:-$DEFAULT_UNIV}
+
+if [ ! -d "bin" ]; then
+  mkdir -p bin
+  echo "Created bin directory"
+else
+  echo "bin directory already exists"
+fi
+
+javac -d bin src/edu/lehigh/swat/bench/uba/*.java
+
+java -cp bin edu.lehigh.swat.bench.uba.Generator -univ $DEFAULT_UNIV -onto https://swat.cse.lehigh.edu/onto/univ-bench.owl
