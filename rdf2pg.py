77d01ade0a8346b9c63b026c898f83e8ba4185e8
zakkii-k <59763423+zakkii-k@users.noreply.github.com>
2024-07-17 23:18:39 +0900

    labelに関するバグの修復と，subClass等の考慮

--- rdf2pg.py
+++ rdf2pg.py
@@ -4,6 +4,8 @@ import glob
 import os
 import tqdm
 import argparse
+import shutil
+from owlready2 import *
 
 
 def parse_arguments():
@@ -51,22 +53,73 @@ def replace_https_with_http(uri):
 def extract_class_info(owl_graph):
     print(f"===== extracting class info =====")
     class_labels = {}
+    subClassOf = {}
     dataTypeProperty = {}
     objectProperty = {}
+    transitiveProperty = {}
+
+    # First, extract all class labels and properties
     for subj, pred, obj in tqdm.tqdm(owl_graph):
         subj = replace_https_with_http(subj)
         pred = replace_https_with_http(pred)
         obj = replace_https_with_http(obj)
 
+        # ブランクノードを無視
+        if isinstance(subj, rdflib.BNode) or isinstance(obj, rdflib.BNode):
+            continue
+
         if pred == rdflib.RDF.type and obj == rdflib.OWL.Class:
             class_labels[str(subj)] = str(subj).split("#")[-1]
         elif pred == rdflib.RDF.type and obj == rdflib.OWL.DatatypeProperty:
             dataTypeProperty[str(subj)] = str(subj).split("#")[-1]
-        elif pred == rdflib.RDF.type and (
-            obj == rdflib.OWL.ObjectProperty or obj == rdflib.OWL.TransitiveProperty
-        ):
+        elif pred == rdflib.RDF.type and (obj == rdflib.OWL.ObjectProperty):
             objectProperty[str(subj)] = str(subj).split("#")[-1]
-    return class_labels, dataTypeProperty, objectProperty
+        elif pred == rdflib.RDF.type and obj == rdflib.OWL.TransitiveProperty:
+            transitiveProperty[str(subj)] = str(subj).split("#")[-1]
+
+    # Next, process subclass and intersectionOf relationships
+    for subj, pred, obj in tqdm.tqdm(owl_graph):
+        subj = replace_https_with_http(subj)
+        pred = replace_https_with_http(pred)
+        obj = replace_https_with_http(obj)
+
+        # ブランクノードを無視
+        if isinstance(subj, rdflib.BNode) or isinstance(obj, rdflib.BNode):
+            continue
+
+        if pred == rdflib.RDFS.subClassOf:
+            if str(subj) not in subClassOf:
+                subClassOf[str(subj)] = []
+            subClassOf[str(subj)].append(str(obj))
+        elif pred == rdflib.OWL.intersectionOf:
+            # Handle intersectionOf to treat it as a subClassOf relationship
+            collection = obj
+            for item in owl_graph.items(collection):
+                if isinstance(item, rdflib.URIRef) and str(item) in class_labels:
+                    if str(subj) not in subClassOf:
+                        subClassOf[str(subj)] = []
+                    subClassOf[str(subj)].append(str(item))
+
+    return (
+        class_labels,
+        subClassOf,
+        dataTypeProperty,
+        objectProperty,
+        transitiveProperty,
+    )
+
+
+def add_inferred_subclasses(owl_file, subClassOf):
+    onto = get_ontology(owl_file).load()
+    with onto:
+        sync_reasoner()
+
+    for cls in onto.classes():
+        for subclass in cls.subclasses():
+            if str(subclass.iri) not in subClassOf:
+                subClassOf[str(subclass.iri)] = []
+            subClassOf[str(subclass.iri)].append(str(cls.iri))
+    return subClassOf
 
 
 def process_node(uri, node_id_map, node_map, next_node_id):
@@ -77,7 +130,39 @@ def process_node(uri, node_id_map, node_map, next_node_id):
     return node_id_map[uri], next_node_id
 
 
-def construct_property_graph(g, class_labels, dataTypeProperty, objectProperty):
+def get_all_super_classes(cls, subClassOf, visited=None):
+    if visited is None:
+        visited = set()
+    if cls in visited:
+        return set()
+    visited.add(cls)
+    super_classes = set()
+    if cls in subClassOf:
+        for super_cls in subClassOf[cls]:
+            super_classes.add(super_cls)
+            super_classes.update(get_all_super_classes(super_cls, subClassOf, visited))
+    return super_classes
+
+
+def get_all_transitive_relations(uri, transitive_relations, visited=None):
+    if visited is None:
+        visited = set()
+    if uri in visited:
+        return set()
+    visited.add(uri)
+    relations = set()
+    if uri in transitive_relations:
+        for related_uri in transitive_relations[uri]:
+            relations.add(related_uri)
+            relations.update(
+                get_all_transitive_relations(related_uri, transitive_relations, visited)
+            )
+    return relations
+
+
+def construct_property_graph(
+    g, class_labels, subClassOf, dataTypeProperty, objectProperty, transitiveProperty
+):
     print(f"===== constructing property graph =====")
     node_map = {}
     edge_map = {}
@@ -86,6 +171,16 @@ def construct_property_graph(g, class_labels, dataTypeProperty, objectProperty):
     next_node_id = 1
     next_edge_id = 1
 
+    transitive_relations = {prop: {} for prop in transitiveProperty}
+    for subj, pred, obj in tqdm.tqdm(g):
+        subj = replace_https_with_http(subj)
+        pred = replace_https_with_http(pred)
+        obj = replace_https_with_http(obj)
+
+        if str(pred) in transitiveProperty:
+            if str(subj) not in transitive_relations[str(pred)]:
+                transitive_relations[str(pred)][str(subj)] = []
+            transitive_relations[str(pred)][str(subj)].append(str(obj))
     for subj, pred, obj in tqdm.tqdm(g):
         subj = replace_https_with_http(subj)
         pred = replace_https_with_http(pred)
@@ -93,7 +188,13 @@ def construct_property_graph(g, class_labels, dataTypeProperty, objectProperty):
         subj = str(subj)
         pred = str(pred)
         obj = str(obj)
-        if "file://" in subj or "file://" in pred or "file://" in obj:
+        if (
+            "file://" in subj
+            or "file://" in pred
+            or "file://" in obj
+            or isinstance(subj, rdflib.BNode)
+            or isinstance(obj, rdflib.BNode)
+        ):
             continue
 
         sub_id, next_node_id = process_node(subj, node_id_map, node_map, next_node_id)
@@ -114,17 +215,43 @@ def construct_property_graph(g, class_labels, dataTypeProperty, objectProperty):
         elif pred in dataTypeProperty:
             node_map[sub_id]["property"][dataTypeProperty[pred]] = obj
         elif "type" in str(pred):
-            if "label" in node_map[sub_id]:
-                node_map[sub_id]["label"].append(class_labels[obj])
-            else:
-                node_map[sub_id]["label"] = [class_labels[obj]]
+            labels = set()
+            if obj in class_labels:
+                labels.add(class_labels[obj])
+            super_classes = get_all_super_classes(obj, subClassOf)
+            for super_cls in super_classes:
+                if super_cls in class_labels:
+                    labels.add(class_labels[super_cls])
+            if labels:
+                if "label" in node_map[sub_id]:
+                    node_map[sub_id]["label"].extend(list(labels))
+                    node_map[sub_id]["label"] = list(set(node_map[sub_id]["label"]))
+                else:
+                    node_map[sub_id]["label"] = list(labels)
+
+    # Handle all transitive relations
+    for prop, relations in transitive_relations.items():
+        for subj, objs in relations.items():
+            subj = str(subj)
+            all_related_objs = get_all_transitive_relations(subj, relations)
+            for related_obj in all_related_objs:
+                if (prop, subj, related_obj) not in edge_id_map:
+                    sub_id = node_id_map[subj]
+                    obj_id = node_id_map[related_obj]
+                    edge_id_map[(prop, subj, related_obj)] = next_edge_id
+                    edge_map[next_edge_id] = {
+                        "id": next_edge_id,
+                        "label": [transitiveProperty[prop]],
+                        "src": sub_id,
+                        "dst": obj_id,
+                        "property": {"uri": prop},
+                    }
+                    next_edge_id += 1
 
     return node_map, edge_map
 
 
 def write_json_in_chunks(data, file_prefix, dir_path, chunk_size=10000):
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
     print(f"===== writing {file_prefix} files =====")
     for i in tqdm.tqdm(range(0, len(data), chunk_size)):
         chunk = data[i : i + chunk_size]
@@ -137,14 +264,24 @@ def main():
     args = parse_arguments()
     owl_graph = load_owl_graph(args.owl_file)
     rdf_graph = load_ntriples_graph(args.ntriples_dir)
-    class_labels, dataTypeProperty, objectProperty = extract_class_info(owl_graph)
+    class_labels, subClassOf, dataTypeProperty, objectProperty, transitiveProperty = (
+        extract_class_info(owl_graph)
+    )
+    subClassOf = add_inferred_subclasses(args.owl_file, subClassOf)
     node_map, edge_map = construct_property_graph(
-        rdf_graph, class_labels, dataTypeProperty, objectProperty
+        rdf_graph,
+        class_labels,
+        subClassOf,
+        dataTypeProperty,
+        objectProperty,
+        transitiveProperty,
     )
 
     node_list = list(node_map.values())
     edge_list = list(edge_map.values())
-
+    if os.path.exists(args.json_dir_path):
+        shutil.rmtree(args.json_dir_path)
+    os.makedirs(args.json_dir_path)
     write_json_in_chunks(node_list, "nodes", args.json_dir_path, args.chunk_size)
     write_json_in_chunks(edge_list, "edges", args.json_dir_path, args.chunk_size)
 
